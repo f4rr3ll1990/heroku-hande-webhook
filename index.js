fb3548829f7abed9f1674b6d27c90ef7527d0b12
F4rr3LL <00mazahaka00@gmail.com>
2021-08-13 18:35:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+
 const router = express.Router();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +11,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 router.post('/test',(request,response) => {
-//code to perform particular action.
-//To access POST variable use req.body()methods.
-console.log(request.body);
-response.end("success!!!");
+	//code to perform particular action.
+	//To access POST variable use req.body()methods.
+	console.log(request.body);
+	response.end("success!!!");
+	var options = { method: 'POST',
+	url: 'https://f4rr3ll-98fc.restdb.io/rest/wikihow',
+	headers: { 'cache-control': 'no-cache',
+	'x-apikey': 'f597576043e3cd21d1e6755024f8fe4821e3b',
+	'content-type': 'application/json' },
+	body: { id: body.result.id, text: body.result.rewrite },
+	json: true
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
 });
 
 // add router in the Express app.
